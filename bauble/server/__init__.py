7acfe0b0ec0e82bba06307e995cc0d10ae028e5d
Brett <brettatoms@gmail.com>
2013-05-26 20:34:23 -0400

    - set cors headers properly

--- bauble/server/__init__.py
+++ bauble/server/__init__.py
@@ -44,9 +44,13 @@ def enable_cors():
     You need to add some headers to each request.
     Don't use the wildcard '*' for Access-Control-Allow-Origin in production.
     """
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Methods'] = 'PUT, GET, POST, DELETE, OPTIONS'
-    response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+    #response.set_header('Access-Control-Allow-Origin', '*')
+    response.set_header('Access-Control-Allow-Origin',
+                        request.headers.get("Origin"))
+    response.set_header('Access-Control-Allow-Methods',
+                        'PUT, GET, POST, DELETE, OPTIONS')
+    response.set_header('Access-Control-Allow-Headers',
+                        'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token, Authorization')
 
 
 @app.get('/test')
