131e7599736af8f29fa1522579bdbd5dbbbdf48d
Brett <brett@bauble.io>
2013-08-27 17:57:48 -0400

    - don't require ~/.bauble.api

--- bauble/__init__.py
+++ bauble/__init__.py
@@ -14,8 +14,6 @@ if os.path.exists(bauble_rcfile) and os.path.isfile(bauble_rcfile):
         name, value = line.split('=')
         if isinstance(name, str) and isinstance(value, str):
             os.environ[name.strip()] = str(value).strip()
-else:
-    raise FileNotFoundError("~/bauble.api does not exist")
 
 if 'DATABASE_URL' not in os.environ:
     raise Exception("DATABASE URL not in environment")
