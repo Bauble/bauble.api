b02b46ed210c65777432dfcea164bf243d1f47ef
Brett <brett@bauble.io>
2013-08-27 17:58:11 -0400

    - cleanup the set admin password script

--- bin/set_admin_password.py
+++ bin/set_admin_password.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import getpass
+import sys
 
 import bauble
 import bauble.i18n
@@ -8,10 +9,9 @@ import bauble.db as db
 from bauble.model import User
 
 username = "admin"
-old_password = getpass.getpass("Old password: ")
-new_password = getpass.getpass("New password: ")
+password = sys.argv[1] if len(sys.argv)>1 else getpass.getpass("New password: ")
 
 session = db.connect()
 user = session.query(User).filter_by(username=username).first()
-user.set_password(new_password)
+user.set_password(password)
 session.commit()
