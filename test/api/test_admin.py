07e8684acbab1d88dc050c0746351ff1f6a3e1cc
Brett <brett@bauble.io>
2013-08-29 21:52:44 -0400

    - disable the initdb test since it's was more of stub

--- test/api/test_admin.py
+++ test/api/test_admin.py
@@ -1,13 +1,13 @@
 
-import test.api as test    
+import test.api as test
 import requests
 import requests.auth as auth
 
-def test_initdb():
+def xtest_initdb():
     admin_url = test.api_root + "/admin"
-    
+
     print('about to request')
-    response = requests.post(admin_url + "/initdb", 
+    response = requests.post(admin_url + "/initdb",
                             auth=auth.HTTPBasicAuth(test.user, test.password))
     print('response: ', response)
     response.raise_for_status()
