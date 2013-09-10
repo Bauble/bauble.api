7942572fdda6a40563633eb7ccbc0ee0a2d31674
Brett <brett@bauble.io>
2013-09-09 22:52:06 -0400

    add stubs for import/export REST handlers

--- bauble/server/admin.py
+++ bauble/server/admin.py
@@ -50,3 +50,13 @@ def initdb():
     finally:
         if session:
             session.close()
+
+
+@app.post(ADMIN_ROOT + "/import")
+def import():
+    pass
+
+
+@app.post(ADMIN_ROOT + "/export/<format>")
+def export(format="csv"):
+    pass
