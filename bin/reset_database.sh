dfe4ad57f7dd03c94297231cf3e5b7dc0d61de7a
Brett <brett@nextglass.co>
2013-12-29 20:27:14 -0500

    make reset_database.sh script work in a fresh environment

--- bin/reset_database.sh
+++ bin/reset_database.sh
@@ -5,12 +5,25 @@ if [ `hostname` == "api.bauble.io" ] ; then
     exit 1
 fi
 
-sudo -u postgres psql -c 'drop database bauble;'
+PSQL="psql"
+SUDO="sudo -u postgres"
+if [[ `uname` == "Darwin" ]] ; then
+    PSQL="/Applications/Postgres93.app/Contents/MacOS/bin/psql"
+    SUDO=""
+fi
+
+$SUDO $PSQL -c 'drop database if exists bauble;'
 if [ $? == 1 ] ; then exit ; fi
 
-sudo -u postgres psql -c 'create database bauble;'
+$SUDO $PSQL -c 'drop role if exists test;'
 if [ $? == 1 ] ; then exit ; fi
-read -p "Press a key when the server is started..."
+
+$SUDO $PSQL -c 'create role test with createdb createrole login;'
+if [ $? == 1 ] ; then exit ; fi
+
+$SUDO $PSQL -c 'create database bauble with owner=test;'
+if [ $? == 1 ] ; then exit ; fi
+read -p "Start the Bauble API server and press a key when it's ready..."
 PYTHONPATH=. bin/initdb.py
 PYTHONPATH=. bin/set_admin_password.py test
 PYTHONPATH=. bin/create_test_org.py
