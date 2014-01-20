02b96622dbfe5d78310da52696c90e84c65c0e16
Brett <brett@nextglass.co>
2014-01-19 20:56:56 -0500

    remove old db.connect usage

--- bin/set_admin_password.py
+++ bin/set_admin_password.py
@@ -9,9 +9,9 @@ import bauble.db as db
 from bauble.model import User
 
 username = "admin"
-password = sys.argv[1] if len(sys.argv)>1 else getpass.getpass("New password: ")
+password = sys.argv[1] if len(sys.argv) > 1 else getpass.getpass("New password: ")
 
-session = db.connect()
+session = db.Session()
 user = session.query(User).filter_by(username=username).first()
 user.set_password(password)
 session.commit()
