e0a2575490c94feca2a965f0a9118fd1929f242c
Brett <brett@nextglass.co>
2014-05-14 19:22:08 -0400

    update the travis build and other cleanups

--- bin/initdb.py
+++ bin/initdb.py
@@ -3,8 +3,11 @@
 # Populate a new empty database.  This script doesn't try to create or drop a new
 # databae so that you need to know what you're doing before you break something.
 #
-# To create a new database use the following command:
+# To create a new database:
 # psql -c "create database $DB_NAME with owner=$DB_OWNER;"
+#
+# To create a new user for the database:
+# psql -c "create database $DB_NAME owner $DB_OWNER encoding 'utf8';" -U postgres
 
 import bauble.db as db
 from bauble.model import SystemModel
