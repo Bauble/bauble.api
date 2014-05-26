bbd5c44f0c0899f0443b3dc3d68d64b3d18dd0f0
Brett <brett@nextglass.co>
2014-05-25 21:24:11 -0400

    add migration script for report table

---
+++ alembic/versions/d2c3abb804_create_report_table.py
@@ -0,0 +1,41 @@
+"""create report table
+
+Revision ID: d2c3abb804
+Revises: 210414005a
+Create Date: 2014-05-25 20:05:53.851881
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'd2c3abb804'
+down_revision = '210414005a'
+
+from alembic import context, op
+import sqlalchemy as sa
+
+import bauble.db as db
+from bauble.model import Report
+
+
+def upgrade():
+    # This migration has to run on a live database and can't be used to generate sql statements.
+    if context.is_offline_mode():
+        raise RuntimeError("This migration script cannot be run in offline mode.")
+
+    # create the report table on all the existing bauble schemas
+    stmt = "select schema_name from information_schema.schemata where schema_name like 'bbl_%%';"
+    for result in db.engine.execute(stmt):
+        schema = result[0]
+        columns = [c.copy() for c in sa.inspect(Report).columns]
+        op.create_table("{}.report".format(schema), *columns)
+
+
+
+def downgrade():
+    if context.is_offline_mode():
+        raise RuntimeError("This migration script cannot be run in offline mode.")
+
+    # drop the report table on all the existing bauble schemas
+    stmt = "select schema_name from information_schema.schemata where schema_name like 'bbl_%%';"
+    for result in db.engine.execute(stmt):
+        op.drop_table("{}.report".format(schema))
