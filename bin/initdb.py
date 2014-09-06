62bd9ec0e245c3aa0cb310f00a9c3a595b343e3a
Brett <brett@nextglass.co>
2014-09-06 16:10:23 -0400

    set alembic version to head with initializing new database

--- bin/initdb.py
+++ bin/initdb.py
@@ -9,9 +9,19 @@
 # To create a new user for the database:
 # psql -c "create database $DB_NAME owner $DB_OWNER encoding 'utf8';" -U postgres
 
+import os
+import traceback
+
+from alembic import command
+from alembic.config import Config
+import sqlalchemy
+
 import bauble.db as db
 from bauble.model import SystemModel
 
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+connection = None
 transaction = None
 try:
     connection = db.engine.connect()
@@ -21,10 +31,19 @@ try:
             raise Exception("Table already exists: {}.  Quitting...".format(table.name))
 
     SystemModel.metadata.create_all(connection)
+
+    # set alembic version to head
+    alembic_cfg = Config(os.path.join(project_root, 'alembic.ini'))
+    command.stamp(alembic_cfg, "head")
+
     transaction.commit()
+except sqlalchemy.exc.CircularDependencyError as exc:
+    for table in exc.cycles:
+        print("** Error creating table: {}".format(table))
+    transaction.rollback()
 except Exception as exc:
     transaction.rollback()
-    print(exc)
+    traceback.print_exc()
 finally:
     if connection:
         connection.close()
