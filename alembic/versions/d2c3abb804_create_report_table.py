4bc325cea26b347134dff142ebe105faaa356596
Brett <brett@nextglass.co>
2014-09-07 19:14:15 -0400

    fix report table migration

--- alembic/versions/d2c3abb804_create_report_table.py
+++ alembic/versions/d2c3abb804_create_report_table.py
@@ -16,9 +16,14 @@ import sqlalchemy as sa
 import bauble.db as db
 from bauble.model import Report
 
+def table_exists(table_name, schema):
+    result = db.engine.execute("select 1 from information_schema.tables where table_name='{}' and table_schema = '{}';".format(table_name, schema)).first()
+    return result is not None
+
 
 def upgrade():
-    # This migration has to run on a live database and can't be used to generate sql statements.
+    # This migration has to run on a live database and can't be used to
+    # generate sql statements.
     if context.is_offline_mode():
         raise RuntimeError("This migration script cannot be run in offline mode.")
 
@@ -26,6 +31,9 @@ def upgrade():
     stmt = "select schema_name from information_schema.schemata where schema_name like 'bbl_%%';"
     for result in db.engine.execute(stmt):
         schema = result[0]
+        if table_exists('report', schema):
+            continue
+
         columns = [c.copy() for c in sa.inspect(Report).columns]
         print("creating table: {}.report".format(schema))
         op.create_table("report", *columns, schema=schema)
@@ -39,4 +47,8 @@ def downgrade():
     # drop the report table on all the existing bauble schemas
     stmt = "select schema_name from information_schema.schemata where schema_name like 'bbl_%%';"
     for result in db.engine.execute(stmt):
-        op.drop_table("report", schema=result[0])
+        schema = result[0]
+        if table_exists('report', schema):
+            continue
+        print("dropping table: {}.report".format(schema))
+        op.drop_table("report", schema=schema)
